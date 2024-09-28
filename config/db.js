1ce0ec1a81ae9974bbdf25f9c74b680f041a61df
lesnansel <kusakalaniosa28@gmail.com>
2024-09-29 03:39:17 +0800

    commit

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '',
+  user: 'ansel',
+  password: 'ansel',
   database: 'ecommerce',
   waitForConnections: true,
   connectionLimit: 10,
